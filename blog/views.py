e2914215f1515a72772eeebd21527b9e9df04980
JohnCua <cuatumax@gmail.com>
2017-08-31 09:20:12 -0600

    Modificacion del archivo listar_pub para mostrar las publicaciones Modificacion del archivo view para llamar los datos del modelo y mandarlo al archivo listar_pub

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 def listar_pub(request):
-    return render(request,'blog/listar_pub.html',{})
+    pub=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request,'blog/listar_pub.html',{'pub':pub})
 
 # Create your views here.
