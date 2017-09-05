38d62e6f574dbfca882723793e6938bf88570754
JohnCua <cuatumax@gmail.com>
2017-09-05 11:39:00 -0600

    implementacion de css creacion de vista publicacion_pub creacion de html detalle_publicacion creacion de archivo blog.css creacion de archivo base.html implementacion de libreria error 404 links de boostrap

--- blog/url.py
+++ blog/url.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listar_pub),
+    url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
 def listar_pub(request):
     pub=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request,'blog/listar_pub.html',{'pub':pub})
 
-# Create your views here.
+
+def detalle_pub(request, pk):
+    p=get_object_or_404(Publicacion,pk= pk)
+    return render(request,'blog/detalle_publicacion.html',{'p':p})
