07a4ba4685b75829556063a4a4e115a3d85c263f
JohnCua <cuatumax@gmail.com>
2017-09-19 12:19:20 -0600

    Nueva vista para un formulario de una nueva publicacion Agregar funcion para editar publicacion seleccionado en la lista

--- blog/url.py
+++ blog/url.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.listar_pub),
     url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
+from .forms import PostForm
+
 def listar_pub(request):
     pub=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request,'blog/listar_pub.html',{'pub':pub})
@@ -9,3 +11,29 @@ def listar_pub(request):
 def detalle_pub(request, pk):
     p=get_object_or_404(Publicacion,pk= pk)
     return render(request,'blog/detalle_publicacion.html',{'p':p})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicacion = timezone.now()
+            post.save()
+            return redirect('postea', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('postea', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
